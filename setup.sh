46f3f3649df1216f565683dba163631f91c8516c
Evan Peterson <evanpeterson@outlook.com>
2018-10-14 12:12:53 -0700

    bootstrap fullpage.js

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 
 npm install
 cp node_modules/normalize.css/normalize.css site/public/css
+cp node_modules/fullpage.js/dist/fullpage.min.css* site/public/css
+cp node_modules/fullpage.js/dist/fullpage.min.js* site/public/js
