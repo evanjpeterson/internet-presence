15573183b97223ee2e5f5a1a0bb311557dd6d2a4
Evan Peterson <evanpeterson@outlook.com>
2019-03-22 20:41:57 -0700

    remove fullpage.js since it seems to make the page lag

--- setup.sh
+++ setup.sh
@@ -2,5 +2,3 @@
 
 npm install
 cp node_modules/normalize.css/normalize.css site/public/css
-cp node_modules/fullpage.js/dist/fullpage.min.css* site/public/css
-cp node_modules/fullpage.js/dist/fullpage.min.js* site/public/js
